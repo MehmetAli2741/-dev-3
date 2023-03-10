c7cc6cd043408e2dba2232d2ebe929909f629839
Mehmet Ali Yilmaz <mehmetaliyilmaz2741@outlook.com>
2023-03-10 06:38:36 +0300

    ödev 3 tamamlandı

---
+++ country_enaz6KarakterVeSonu_n_.sql
@@ -0,0 +1,6 @@
+--SELECT *FROM country
+--WHERE country LIKE 'A%a';
+
+SELECT *FROM country
+WHERE country LIKE '_____%n';
+
---
+++ film_title_enaz4karekter_T_harfiOlan.sql
@@ -0,0 +1,8 @@
+--SELECT *FROM country
+--WHERE country LIKE 'A%a';
+
+--SELECT *FROM country
+--WHERE country LIKE '______%n';
+
+SELECT title FROM film
+WHERE title ILIKE  '___T%';
